f1084276d7cc88b36c78d45341f8b834761e23d5
becardine <brenda_alves@live.com>
2022-04-07 22:33:43 -0300

    list data in table

--- app.js
+++ app.js
@@ -111,6 +111,32 @@ function carregaListaDespesas() {
   let despesas = Array()
   despesas = bd.recuperarTodosRegistros()
 
-  console.log(despesas)
+  //selecionando o elemento tbody
+  let listaDespesas = document.getElementById('listaDespesas')
+
+  //percorrer o array despesas, listando cada despesa de forma dinamica
+  despesas.forEach(function(d){
+    //criando a linha (tr)
+    let linha = listaDespesas.insertRow()
+
+    //inserir valores (td)
+    linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+    switch(d.tipo) {
+      case '1': d.tipo = 'Alimentação'
+      break
+      case '2': d.tipo = 'Educação'
+      break
+      case '3': d.tipo = 'Lazer'
+      break
+      case '4': d.tipo = 'Saúde'
+      break
+      case '5': d.tipo = 'Transporte'
+      break
+    }
+    linha.insertCell(1).innerHTML = d.tipo
+
+    linha.insertCell(2).innerHTML = d.descricao
+    linha.insertCell(3).innerHTML = d.valor
+  })
 
 }
