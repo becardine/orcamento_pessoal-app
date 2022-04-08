8773565345f782264321069c3dc7f0ba1799ee12
becardine <brenda_alves@live.com>
2022-04-08 20:32:18 -0300

    delete expense

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ class Bd {
       if (despesa === null) {
         continue;
       }
+      despesa.id = i
       despesas.push(despesa);
     }
 
@@ -89,6 +90,10 @@ class Bd {
     }
 
     return despesasFiltradas;
+  }
+
+  remover(id) {
+    localStorage.removeItem(id)
   }
 }
 
@@ -183,6 +188,21 @@ function carregaListaDespesas(despesas = Array(), filtro = false) {
 
     linha.insertCell(2).innerHTML = d.descricao;
     linha.insertCell(3).innerHTML = d.valor;
+
+    //criar o botão de exclusão
+    let btn = document.createElement('button')
+    btn.className = 'btn btn-danger'
+    btn.innerHTML = '<i class="fas fa-times"></i>'
+    btn.id = `id_despesa_${d.id}`
+    btn.onclick = function() {
+
+      let id = this.id.replace('id_despesa_', '')
+      bd.remover(id)
+      window.location.reload()
+    }
+    linha.insertCell(4).append(btn)
+
+    console.log(d)
   });
 }
 
